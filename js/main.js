9945ab5158cc1241013606b7775ef218aad07d6d
Hossam Adel <122458767+Hossam592@users.noreply.github.com>
2023-06-13 15:21:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const products =[
+    {
+        id:0,
+        Title:"Tank top",
+        price:"50$",
+        img:"Images/cloth_1.jpg",
+    },
+    {
+        id:1,
+        Title:"Coater",
+        price:"30$",
+        img:"Images/shoe.png",
+    },
+    {
+        id:2,
+        Title:"Polo Shirt",
+        price:"20$",
+        img:"Images/cloth_2.jpg",
+    },
+    {
+        id:3,
+        Title:"T-Shirt",
+        price:"25$",
+        img:"Images/cloth_3.jpg",
+    },
+    {
+        id:4,
+        Title:"Coater",
+        price:"30$",
+        img:"Images/shoe.png",
+    },
+    {
+        id:5,
+        Title:"Tank top",
+        price:"50$",
+        img:"Images/cloth_1.jpg",
+    },
+    {
+        id:6,
+        Title:"T-Shirt",
+        price:"25$",
+        img:"Images/cloth_3.jpg",
+    },
+    {
+        id:7,
+        Title:"Polo Shirt",
+        price:"20$",
+        img:"Images/cloth_2.jpg",
+    },
+    {
+        id:8,
+        Title:"Tank top",
+        price:"50$",
+        img:"Images/cloth_1.jpg",
+    },
+]
+
+// Render items
+let Product_list =document.querySelector('.Products')
+let CurrentItem = ""
+function renderItems(){
+    products.forEach(product =>{
+        CurrentItem += `
+        <div class="col rounded-3 product">
+        <div class="card">
+            <img src="${product.img}" class="card-img-top" width="100%" height="200px" alt="...">
+            <div class="card-body shop_product_txt">
+                <a href="product.html"><p>${product.Title}</p></a>
+                <p class="p2">Finding perfect product</p>
+                <p>${product.price}</p>
+            </div>
+        </div>
+        </div>
+        `
+        Product_list.innerHTML = CurrentItem
+    })
+}renderItems()
