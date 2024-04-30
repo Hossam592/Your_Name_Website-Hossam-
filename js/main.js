225d67da86724c6361769980d4335ac25b98537f
Hossam Adel <122458767+HossamAdelAli@users.noreply.github.com>
2024-04-30 15:45:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,15 +19,15 @@ const products =[
     },
     {
         id:3,
-        Title:"T-Shirt",
-        price:"25$",
-        img:"Images/cloth_3.jpg",
-    },
-    {
-        id:4,
         Title:"Coater",
         price:"30$",
         img:"Images/shoe.png",
+    },
+    {
+        id:4,
+        Title:"T-Shirt",
+        price:"25$",
+        img:"Images/cloth_3.jpg",
     },
     {
         id:5,
@@ -43,35 +43,35 @@ const products =[
     },
     {
         id:7,
-        Title:"Polo Shirt",
-        price:"20$",
-        img:"Images/cloth_2.jpg",
-    },
-    {
-        id:8,
         Title:"Tank top",
         price:"50$",
         img:"Images/cloth_1.jpg",
     },
+    {
+        id:8,
+        Title:"Polo Shirt",
+        price:"20$",
+        img:"Images/cloth_2.jpg",
+    }
 ]
 
 // Render items
-let Product_list =document.querySelector('.Products')
+let Products_list =document.querySelector('.Products')
 let CurrentItem = ""
 function renderItems(){
     products.forEach(product =>{
         CurrentItem += `
         <div class="col rounded-3 product">
         <div class="card">
-            <img src="${product.img}" class="card-img-top" width="100%" height="200px" alt="...">
+            <img src="${product.img}" class="card-img-top product_img" width="100%" height="200px" alt="...">
             <div class="card-body shop_product_txt">
-                <a href="product.html"><p>${product.Title}</p></a>
+                <a href="product.html" class="product_title"><p>${product.Title}</p></a>
                 <p class="p2">Finding perfect product</p>
-                <p>${product.price}</p>
+                <p class="product_price">${product.price}</p>
             </div>
         </div>
         </div>
         `
-        Product_list.innerHTML = CurrentItem
+        Products_list.innerHTML = CurrentItem
     })
 }renderItems()
